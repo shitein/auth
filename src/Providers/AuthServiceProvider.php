71cf5836e0abcb4e0d0370ba99127e7e1d4614d0
shitein <98593768+shitein@users.noreply.github.com>
2022-01-28 21:44:27 +0530

    Update AuthServiceProvider.php

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -8,8 +8,8 @@ class AuthServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '../src/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '../src/resources/views', 'auth');
+        //$this->loadRoutesFrom(__DIR__ . '../src/routes/web.php');
+        //$this->loadViewsFrom(__DIR__ . '../src/resources/views', 'auth');
         //$this->loadMigrationsFrom(__DIR__.'../../src/database/migrations');
 
         /*$this->publishes([
