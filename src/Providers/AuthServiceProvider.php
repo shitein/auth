927e5650fb1564aadba9d29e9153c848f1e3e9f8
shitein <98593768+shitein@users.noreply.github.com>
2022-01-28 18:16:18 +0530

    Create AuthServiceProvider.php

---
+++ src/Providers/AuthServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Collab\Auth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register() 
+    {
+
+    }
+}
