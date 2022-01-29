e29516d21689ab8400e6bede937db6b4371907b9
shitein <98593768+shitein@users.noreply.github.com>
2022-01-29 14:46:03 +0530

    Update AuthServiceProvider.php

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -17,7 +17,7 @@ class AuthServiceProvider extends ServiceProvider
         ], 'public');
         
         $this->publishes([
-            __DIR__.'/resources/views' => public_path('resources/vendor/shite-auth/views'),
+            __DIR__.'/resources' => public_path('vendor/shite-auth/views'),
         ], 'views');
     }
 
